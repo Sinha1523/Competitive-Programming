9f32ed09d218d4c33ada6756a9aa74ff1ca1f79d
Sinha A. <bloggersinha152@gmail.com>
2025-04-22 00:19:34 +0800

    D - 1D Eraser.cpp

---
+++ D - 1D Eraser.cpp
@@ -0,0 +1,34 @@
+//week 4 XPSC Club
+//https://codeforces.com/contest/1873/problem/D
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+
+        int ans = 0;
+        int i = 0;
+
+        while (i < n) {
+            if (s[i] == 'B') {
+                ans++;
+                i += k;
+            } else {
+                i++;
+            }
+        }
+
+        cout << ans << endl;
+    }
+    return 0;
+}
