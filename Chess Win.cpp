717969608592567b8de589f862c092b25b953836
Sinha A. <bloggersinha152@gmail.com>
2025-05-14 20:57:11 +0600

    Chess Win.cpp

---
+++ Chess Win.cpp
@@ -0,0 +1,16 @@
+//practice contest in codechef
+//https://www.codechef.com/START186D/problems/CHESSWIN
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int A, B;
+    cin >> A >> B;
+    cout << (B - A + 1) << endl;
+
+    return 0;
+}
